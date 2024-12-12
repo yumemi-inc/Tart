798c2548a633c53f8c34822aac876f385254bcd8
Hiroyuki Kusu <h_kusu@yumemi.co.jp>
2024-12-12 21:35:53 +0900

    Change MessageMiddleware interface

--- tart-message/src/commonMain/kotlin/io/yumemi/tart/message/MessageMiddleware.kt
+++ tart-message/src/commonMain/kotlin/io/yumemi/tart/message/MessageMiddleware.kt
@@ -6,17 +6,27 @@ import io.yumemi.tart.core.Middleware
 import io.yumemi.tart.core.State
 import io.yumemi.tart.core.Store
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 interface Message
 
+private object MessageHub {
+    private val _messages = MutableSharedFlow<Message>()
+    val messages: Flow<Message> get() = _messages
+
+    suspend fun send(message: Message) {
+        _messages.emit(message)
+    }
+}
+
 @Suppress("unused")
 abstract class MessageMiddleware<S : State, A : Action, E : Event> : Middleware<S, A, E> {
     final override suspend fun onInit(store: Store<S, A, E>, coroutineContext: CoroutineContext) {
         CoroutineScope(coroutineContext).launch {
-            messages.collect {
+            MessageHub.messages.collect {
                 receive(it, store::dispatch)
             }
         }
@@ -36,11 +46,9 @@ abstract class MessageMiddleware<S : State, A : Action, E : Event> : Middleware<
     final override suspend fun afterStateChange(state: S, prevState: S) {}
     final override suspend fun beforeError(state: S, error: Throwable) {}
     final override suspend fun afterError(state: S, nextState: S, error: Throwable) {}
+}
 
-    companion object {
-        private val messages = MutableSharedFlow<Message>()
-        suspend fun send(message: Message) {
-            messages.emit(message)
-        }
-    }
+@Suppress("unused")
+suspend fun send(message: Message) {
+    MessageHub.send(message = message)
 }
