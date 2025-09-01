bf1116c2bdda3cf9ab30a6b02691c239dcdca139
TBSten <programmingCafeteria@gmail.com>
2025-09-01 21:57:23 +0900

    add KDoc

--- tart-viewmodel/src/commonMain/kotlin/io/yumemi/tart/viewmodel/StoreViewModel.kt
+++ tart-viewmodel/src/commonMain/kotlin/io/yumemi/tart/viewmodel/StoreViewModel.kt
@@ -6,7 +6,28 @@ import io.yumemi.tart.core.Action
 import io.yumemi.tart.core.Event
 import io.yumemi.tart.core.State
 import io.yumemi.tart.core.StoreBuilder
+import io.yumemi.tart.core.Store
 
+/**
+ * A [ViewModel] with a [store][StoreViewModel.store] property that is its own dedicated [Store].
+ * The [store][StoreViewModel.store] property is created by [ViewModelStore], enabling it to offer features more tightly integrated with the [ViewModel].
+ * (For details, refer to the [ViewModelStore] documentation.)
+ *
+ * # Usage
+ *
+ * ```kt
+ * class MyViewModel : StoreViewModel({
+ *   // TODO
+ * })
+ *
+ * val viewModel: MyViewModel = // ...
+ * viewModel.store /* : Store<...> */
+ * ```
+ *
+ * @param storeKey The key for the [Store] held within the [ViewModel]. A Store instance is created within the [ViewModel] for each of these keys.
+ * @param builder Configuration block to customize the [Store]
+ * @property store
+ */
 abstract class StoreViewModel<S: State, A: Action, E: Event>(
     storeKey: String,
     builder: StoreBuilder<S, A, E>.() -> Unit,
--- tart-viewmodel/src/commonMain/kotlin/io/yumemi/tart/viewmodel/ViewModelExt.kt
+++ tart-viewmodel/src/commonMain/kotlin/io/yumemi/tart/viewmodel/ViewModelExt.kt
@@ -10,6 +10,26 @@ import io.yumemi.tart.core.StoreBuilder
 
 internal const val TART_STORE_KEY = "io.yumemi.tart.ViewModelStore"
 
+/**
+ * A helper function for creating a [Store] bound to a [ViewModel].
+ * The Store being created will feature greater integration with the ViewModel, as shown below.
+ *
+ * - Automatically [dispose()][Store.dispose] of when [ViewModel.onCleared()][ViewModel.onCleared] is called.
+ * - The viewModelScope.coroutineContext is set to the coroutineContext.
+ *
+ * # Usage
+ *
+ * ```kt
+ * class MyViewModel : ViewModel() {
+ *   val store = ViewModelStore {
+ *     // TODO
+ *   }
+ * }
+ * ```
+ *
+ * @param key The key for the [Store] held within the [ViewModel]. A Store instance is created within the [ViewModel] for each of these keys.
+ * @param builder Configuration block to customize the [Store]
+ */
 @Suppress("FunctionName")
 fun <S: State, A: Action, E: Event> ViewModel.ViewModelStore(
     key: String = TART_STORE_KEY,
